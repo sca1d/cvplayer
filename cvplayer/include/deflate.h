99335072d797dfa177e93a9e63e338881a521018
sca1d <lol.scald.lol@gmail.com>
2022-02-20 18:15:49 +0900

    file setup

---
+++ cvplayer/include/deflate.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc/imgproc_c.h"
+
+#pragma comment(lib,"comctl32.lib")
+#pragma comment(lib,"vfw32.lib")
+
+#define DLL_EXPORT	__declspec(dllexport)
